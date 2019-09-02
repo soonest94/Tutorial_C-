799287656267c355f28d4bc4eb23b4771c39f5f9
soonest94 <soonest94@gmail.com>
2019-09-03 02:50:03 +0900

    Circulate kinds of counting systems

---
+++ CountingSystem.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int x=100;
+
+  printf("100 is %d as decimal counting system. \n", x);
+  printf("100 is %o as octal counting system. \n", x);
+  printf("100 is %x as hexadecimal counting system. \n", x);
+  return 0;
+}
