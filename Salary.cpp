729de35a46503d59ef35fa8dfb9f29d2ef1281a7
soonest94 <soonest94@gmail.com>
2019-09-03 02:28:00 +0900

    Circulating with a constant number

---
+++ Salary.cpp
@@ -0,0 +1,12 @@
+//constant number
+
+#include <stdio.h>
+#define MONTH 12
+//Declare const. number 
+
+int main(void)
+{
+  double monthSalary=20.5;
+  printf("JPY %.2f", monthSalary*MONTH);
+  return 0;
+}
