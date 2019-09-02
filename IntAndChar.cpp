25576488e55824d72a51167fb00096e553199a1d
soonest94 <soonest94@gmail.com>
2019-09-03 02:35:23 +0900

    Data type, int and char

---
+++ IntAndChar.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+  char x='A';
+  printf("%c \n", x);
+
+  int y=65;
+  printf("%c \n", y);
+  //Ascii code 'A'=65
+
+  char z='B';
+  printf("%c \n", z);
+  printf("%d \n", z);
+  return 0;
+}
