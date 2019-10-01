94a22a55c88719ee10bf7bd14c399f7f27315892
soonest94 <soonest94@gmail.com>
2019-10-01 14:39:27 +0900

    Compare struct

---
+++ compare_struct.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+struct point {
+  int x;
+  int y;
+};
+
+void comparePoint(struct point p1, struct point p2) {
+  if((p1.x == p2.x) && (p2.y == p2.y))
+  {
+    printf("p1 is same with p2");
+  }
+}
+
+int main(void)
+{
+  struct point p1;
+  struct point p2;
+
+  p1.x = 30;
+  p1.y = 10;
+
+  p2.x = 30;
+  p2.y = 10;
+
+  /*
+  if(p1 == p2)
+  {
+    printf("p1 is same with p2);
+  }
+  */
+
+  comparePoint(p1, p2);
+  return 0;
+}
